e9cca27e5dc43f5afbddc607c764a91f5718dfe1
dqzs <163081196+dqzs@users.noreply.github.com>
2024-11-22 20:02:45 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -12,8 +12,7 @@ import tempfile
 st.markdown(
     """
     <div style='text-align: center;'>
-        <h1>Predict Fluorescence Emission Wavelengths</h1>
-        <h3 style='color: #FF6347;'>Emission Wavelength Prediction Based on Molecular Structure</h3>
+        <h1>Predict Fluorescence Emission Wavelengths Based on Molecular Structure</h1>
         <blockquote style='margin: auto; width: 70%; background: #f9f9f9; border-left: 10px solid #ccc; padding: 10px; font-size: 1.1em;'>
             This tool aims to quickly predict the emission wavelengths of molecules based on their structures (SMILES or SDF files) using molecular descriptors and machine learning models.
         </blockquote>
