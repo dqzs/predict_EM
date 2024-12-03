138accf2e2ff0df3579c0af0cf1694b0da25a5e3
dqzs <163081196+dqzs@users.noreply.github.com>
2024-12-03 15:36:56 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -11,14 +11,6 @@ import tempfile
 st.markdown(
     """
     <style>
-    .rounded-container {
-        border: 2px solid #000000; /* 黑色边框 */
-        border-radius: 20px; /* 圆角边框 */
-        padding: 15px; /* 内边距 */
-        margin: 10px auto; /* 外边距，居中 */
-        max-width: 80%; /* 最大宽度，适配窗口 */
-        background-color: #f9f9f9; /* 背景颜色 */
-    }
     .stApp {
         background-color: #ffffff; /* 页面背景 */
     }
