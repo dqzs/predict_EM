5107587d18f0cf544a99e6de411280ce60796a63
dqzs <163081196+dqzs@users.noreply.github.com>
2024-12-03 15:16:26 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -14,8 +14,8 @@ st.markdown(
     .stApp {
         border: 2px solid #000000; /* 黑色边框 */
         border-radius: 20px; /* 圆角边框 */
-        padding: 20px; /* 内边距 */
-        margin: 200px; /* 外边距 */
+        padding: 200px; /* 内边距 */
+        margin: 20px; /* 外边距 */
     }
     </style>
     """,
