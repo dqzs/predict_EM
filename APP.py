e601b45ea49390c46ea68f0edf18c5dd71cd95de
dqzs <163081196+dqzs@users.noreply.github.com>
2024-12-04 21:09:34 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -13,7 +13,6 @@ st.markdown(
     .stApp {
         border: 2px solid #808080;
         border-radius: 20px;
-        padding: 15px;
         margin: 50px auto; /* 调整顶部外边距 */
         max-width: 39%; /* 使用百分比而不是固定值 */
         background-color: #f9f9f9;
@@ -24,7 +23,7 @@ st.markdown(
             max-width: 95%; /* 在小屏幕上使用全宽 */
         }
     }
-    .rounded-container h3 {
+    .rounded-container h1 {
         text-align: center; /* 确保标题文本居中 */
         background-color: #e0e0e0; /* 标题背景颜色 */
         padding: 10px; /* 标题内边距 */
@@ -35,7 +34,6 @@ st.markdown(
         background-color: #f0f0f0; /* 块引用背景颜色 */
         padding: 10px; /* 块引用内边距 */
         font-size: 1.1em; /* 字体大小 */
-        max-width: 95%; /* 最大宽度 */
     }
     a {
         color: #0000EE; /* 链接颜色 */
@@ -50,7 +48,7 @@ st.markdown(
 st.markdown(
     """
     <div class='rounded-container'>
-        <h3>Predict Organic Fluorescence Emission Wavelengths</h3>
+        <h1>Predict Organic Fluorescence Emission Wavelengths</h1>
         <blockquote>
             1. This website aims to quickly predict the emission wavelength of organic molecules based on their structure (SMILES or SDF files) using machine learning models.<br>
             2. It is recommended to use ChemDraw software to draw the molecular structure and convert it to sdf.<br>
