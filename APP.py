6fa5d7c6b13f3b79dd24aa5ccd14e7e898c0911c
dqzs <163081196+dqzs@users.noreply.github.com>
2024-12-04 21:13:43 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -24,7 +24,7 @@ st.markdown(
         }
     }
     .rounded-container h2 {
-        margin: -30px auto; /* 调整顶部外边距 */
+        margin-top: -30px; /* 调整顶部外边距 */
         text-align: center; /* 确保标题文本居中 */
         background-color: #e0e0e0; /* 标题背景颜色 */
         padding: 10px; /* 标题内边距 */
