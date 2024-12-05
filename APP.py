ba8e6a98c7eba9e80c17b0bc34eb6ef5bdedc139
dqzs <163081196+dqzs@users.noreply.github.com>
2024-12-05 16:17:20 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -19,7 +19,7 @@ st.markdown(
     padding-bottom: 0px; /* 减少底部的间距 */
     }
     .dataframe-container {
-        margin-bottom: 0px; /* 减少表格和底部内容的间距 */
+        margin-bottom: -80px; /* 减少表格和底部内容的间距 */
     }
     .rounded-container h2 {
         margin-top: -80px;
